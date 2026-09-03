a996f6c253721d533b8b8b64d87182ac02e7e3f3
Boris Anthony <964649+BorisAnthony@users.noreply.github.com>
2026-09-03 13:52:35 +0200

    Cherry-picked 1.17.5 fixes
    Only those changes that
    - addressed the Calibre 9 bug
    - added missing files ("run.cmd" and a copy of "annotated_books")
    - introduction of "r" (Raw) regex flag, which suppresses warnings.
    
    NOT brought in:
    - apparent regressions when compared to last davidfor repo commit.
    e.g.:
    - Color map additions
    - Pocketbook colormap additions

--- annotations.py
+++ annotations.py
@@ -96,7 +96,7 @@ class Annotations(Annotation, Logger):
             key = self._timestamp_to_datestr(annotation.timestamp)
             MONTHS = [None, 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
                             'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-            if not re.match('\d{2} \w{3} \d{4} \d{2}:\d{2}:\d{2}', key):
+            if not re.match(r'\d{2} \w{3} \d{4} \d{2}:\d{2}:\d{2}', key):
                 return "!%s" % key
             sts_elems = key[1:-1].split(' ')
             year = sts_elems[2]
